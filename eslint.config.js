3112e5ef26490e724276a57fcdee686bab8a1ad8
Nikita Karamov <me@kytta.dev>
2026-02-08 23:24:25 +0100

    Migrate to flat config

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const globals = require("globals");
+const js = require("@eslint/js");
+const nodePlugin = require("eslint-plugin-n");
+const eslintPlugin = require("eslint-plugin-eslint-plugin");
+
+module.exports = [
+    {
+        languageOptions: { globals: globals.mocha },
+    },
+	js.configs.recommended,
+	nodePlugin.configs["flat/recommended-script"],
+	eslintPlugin.configs["flat/recommended"],
+];
