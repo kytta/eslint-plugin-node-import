aed15fe6b7b593b257c80ce0e8fcb0d67a6bec8e
Nikita Karamov <me@kytta.dev>
2026-02-08 23:45:08 +0100

    Dogfood

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ const globals = require("globals");
 const js = require("@eslint/js");
 const nodePlugin = require("eslint-plugin-n");
 const eslintPlugin = require("eslint-plugin-eslint-plugin");
+const nodeImportPlugin = require("./lib/index.js");
 
 module.exports = [
     {
@@ -12,4 +13,5 @@ module.exports = [
 	js.configs.recommended,
 	nodePlugin.configs["flat/recommended-script"],
 	eslintPlugin.configs["flat/recommended"],
+	...nodeImportPlugin.configs["flat/recommended"],
 ];
