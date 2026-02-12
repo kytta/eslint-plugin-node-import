81c090ef56da513dd6c02cbe29e460e22a96823b
Nikita Karamov <me@kytta.dev>
2026-02-12 08:08:51 +0100

    Simplify type definitions

--- lib/rules/prefer-node-protocol.d.ts
+++ lib/rules/prefer-node-protocol.d.ts
@@ -1,17 +1,5 @@
 import { Rule } from "eslint";
 
-declare const rule: Rule.RuleModule & {
-	meta: {
-		type: string,
-		docs: {
-			description: string,
-			recommended: true,
-			url: string,
-		},
-		fixable: string,
-		schema: [],
-		messages: { [messageId: string]: string },
-	}
-};
+declare const rule: Rule.RuleModule & { meta: { schema: [] } };
 
 export = rule;
